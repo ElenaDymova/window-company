e46803bede494bfff7a0dd8b329242b4e92964ce
Elena <ebe2017@yandex.ru[remote origin]>
2023-02-18 19:44:46 +0300

    improved our code and added opening the window 30 minutes later

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,16 +1,23 @@
 const modals = () => {
 	//функция отвечающая за привязку модального окна к триггеру
-	function bindModal(trigger, modal, close) {
+	function bindModal(triggerSelector, modalSelector, closeSelector) {
 		//аргументы - триггер (селектор кнопки), модальное окно кот. буд. открывать, селектор закрытия модального окна
-		trigger.addEventListener('click', (e) => {
-			if (e.target) { //проверяем существование ивента
-				e.preventDefault(); //отменяем стандартное поведение браузера
-			}
 
-			modal.style.display = "block"; //показывает модальное окно
-			document.body.style.overflow = "hidden"; //отменяет пролистывание страницы при открытом модальном окне
+		const trigger = document.querySelectorAll(triggerSelector),
+			modal = document.querySelector(modalSelector),
+			close = document.querySelector(closeSelector);
+
+		trigger.forEach(item => {
+			item.addEventListener('click', (e) => {
+				if (e.target) { //проверяем существование ивента
+					e.preventDefault(); //отменяем стандартное поведение браузера
+				}
+
+				modal.style.display = "block"; //показывает модальное окно
+				document.body.style.overflow = "hidden"; //отменяет пролистывание страницы при открытом модальном окне
+			});
+
 		});
-
 		//закрытие окна
 		close.addEventListener('click', () => {
 			modal.style.display = "none";
@@ -27,14 +34,17 @@ const modals = () => {
 
 	}
 
-	const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
-		//кнопка вызова
-		modalEngineer = document.querySelector('.popup_engineer'),
-		//попап
-		modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
-	//кнопка закытия
+	//появление окна через 60 сек
+	function showModalByTime(selector, time) {
+		setTimeout(function () {
+			document.querySelector(selector).style.display = "block";
+			document.body.style.overflow = "hidden";
+		}, time);
+	}
 
-	bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+	bindModal('.phone_link', '.popup', '.popup .popup_close');
+	showModalByTime('.popup', 3000);
 
 };
 
