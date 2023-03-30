ccab7439808e81a114d5d9bbc522551f0d0d6d8a
Elena <ebe2017@yandex.ru[remote origin]>
2023-03-30 18:23:02 +0300

    improve job forms

---
+++ src/js/modules/forms.js
@@ -0,0 +1,60 @@
+const forms = () => {
+	const form = document.querySelectorAll('form'), //получаем все формы кот есть на странице
+		inputs = document.querySelectorAll('input'); //получаем все инпуты
+
+	const message = {
+		loading: 'Загрузка...',
+		sucsess: 'Спасибо! Скоро мы с вами свяжемся',
+		failure: 'Что-то пошло не так...'
+	};
+
+	//функция отвечающ за отправку запроса
+	const postData = async (url, data) => {
+		document.querySelector('.status').textContent = message.loading;
+		let res = await fetch(url, {
+			method: "POST",
+			body: data
+		});
+
+		return await res.text();
+
+	};
+
+	const clearInputs = () => {
+		inputs.forEach(item => {
+			item.value = '';
+		});
+	};
+
+	form.forEach(item => { //навесили на каждую форму внутри обработчик события
+		item.addEventListener('submit', (e) => { //на каждый айтем навесили обраотчик событий submit, внутри объект события
+			e.preventDefault();//при отправке данных страница перезагружаться не будет
+
+			//создаем блок для сообщения
+			let statusMessage = document.createElement('div');
+			statusMessage.classList.add('status');//добавили класс
+			//помещаем блок на страницу
+			item.appendChild(statusMessage); //бберем ту форму кот сейчас перебираем - item
+			//и воспользуемся методом appendChild - помещаем блок в конец нашей формы
+
+			//собирае данные, кот есть в форме
+			const formData = new FormData(item); //создаем переменную, внутри конструктор; (item) - откуда беремеданные
+			//этот объект найдет все импуты. соберет все данные в спец структуру и поместит в переменную
+
+			postData('assets/server.php', formData)
+				.then(res => {
+					console.log(res);
+					statusMessage.textContent = message.sucsess;
+				})
+				.catch(() => statusMessage.textContent = message.failure)
+				.finally(() => {
+					clearInputs();
+					setTimeout(() => {
+						statusMessage.remove();
+					}, 5000);
+				});
+		});
+	});
+};
+
+export default forms;
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,49 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+	const header = document.querySelector(headerSelector),
+		tab = document.querySelectorAll(tabSelector),
+		content = document.querySelectorAll(contentSelector);
+
+	//скрывает контент
+	function hideTabContent() {
+		content.forEach(item => {
+			item.style.display = 'none';
+		}); //перебрали все элементы и скрыли каждый из них
+
+		//убирает классы активности у ненужных табов
+		tab.forEach(item => {
+			item.classList.remove(activeClass);
+		});
+	}
+
+
+	//показывает контент
+	function showTabContent(i = 0) { //аргумент чтобы показать какой-то конкретный элемент
+		content[i].style.display = 'block';
+		//добавляет класс активности
+		tab[i].classList.add(activeClass);
+	}
+
+	hideTabContent();
+	showTabContent();
+
+	//отслеживаем, в какой таб кликнул пользователь, используем делегирование событий
+	header.addEventListener('click', (e) => {
+		const target = e.target; //переменная с элементом, на кот. произошло событие (куда кликнул пользователь)
+		//удостоверяемся, точно ли пользователь клинкнул туда, куда мы задумали
+		//точно ли кликнул в таб, в не в пробел между ними или т.п.
+		if (target &&
+			(target.classList.contains(tabSelector.replace(/\./, "")) || //проверяем тоже самое только у родителя
+				//определяем в какой именно по счету элемент кликнули
+				target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+			tab.forEach((item, i) => { //item каждый элемент кот перебираем, i - номер перебираемого элемента
+				if (target == item || target.parentNode == item) { //если тот элемент в кот кликнул пользователь будет равен 
+					//тому элемеенту кот сейчас перебирается в этом цикле или родитель будет равен этому item
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+};
+
+export default tabs;
